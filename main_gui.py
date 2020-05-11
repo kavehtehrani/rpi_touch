75e81a0f691b322850aeb7aa1b89f4574f8cf578
Kaveh <kaveh_tehrani@yahoo.com>
2020-05-10 23:30:26 -0400

    smol

--- main_gui.py
+++ main_gui.py
@@ -18,6 +18,7 @@ class RPIE_GUI:
         self.frame.pack()
         self.create_gui()
         self.r = reddit.rpie_reddit()
+        self.wnd_joke = None
 
     def create_gui(self):
         self.l_entries = []
@@ -32,15 +33,43 @@ class RPIE_GUI:
         self.bt_update = ttk.Button(self.frame, text="Dad Joke!", command=self.gui_dad_joke)
         self.bt_update.grid(row=2, column=0, columnspan=1)
 
+        self.bt_update = ttk.Button(self.frame, text="Smol!", command=self.gui_smolcat)
+        self.bt_update.grid(row=3, column=0, columnspan=1)
+
     def gui_dad_joke(self):
         str_joke, str_punchline = self.r.get_dadjoke()
-        self.pop_text_window(f"{str_joke}...\n{str_punchline}")
+        self.pop_text_window(f"{str_joke}...\n\n{str_punchline}")
+
+    def gui_smolcat(self):
+        self.r.get_smol_cat()
 
     def pop_text_window(self, str_text):
-        t = tk.Toplevel(self.master)
-        l = ttk.Label(t, text=str_text)
+        if self.wnd_joke:
+            self.wnd_joke.destroy()
+
+        self.wnd_joke = tk.Toplevel(self.master)
+        self.center(self.wnd_joke)
+        l = ttk.Label(self.wnd_joke, text=str_text, wrap=250)
         l.grid(row=0, column=0)
 
+    def center(toplevel):
+        toplevel.update_idletasks()
+
+        # Tkinter way to find the screen resolution
+        # screen_width = toplevel.winfo_screenwidth()
+        # screen_height = toplevel.winfo_screenheight()
+
+        # PyQt way to find the screen resolution
+        app = QtGui.QApplication([])
+        screen_width = app.desktop().screenGeometry().width()
+        screen_height = app.desktop().screenGeometry().height()
+
+        size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
+        x = screen_width/2 - size[0]/2
+        y = screen_height/2 - size[1]/2
+
+        toplevel.geometry("+%d+%d" % (x, y))
+        toplevel.title("Centered!")
 
 if __name__ == '__main__':
     root = ThemedTk(themebg=True)
--- reddit.py
+++ reddit.py
@@ -1,6 +1,10 @@
 import praw
 import datetime
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import urllib.request, io, base64, tkinter
 
 # r = praw.Reddit(user_agent='my_cool_application')
 # submissions = r.get_subreddit('opensource').get_hot(limit=5)
@@ -28,8 +32,35 @@ class rpie_reddit:
 
         return hot_posts[nrand].title, hot_posts[nrand].selftext
 
+
+    def get_smol_cat(self):
+        hot_posts = list(self.r.subreddit('IllegallySmolCats').hot(limit=self.n_length_hot))
+
+        while True:
+            nrand = random.randrange(self.n_length_hot)
+            if '.jpg' in hot_posts[nrand].url or '.gif' in hot_posts[nrand].url:
+                break
+                # return hot_posts[nrand].url
+
+        path = io.BytesIO(urllib.request.urlopen(hot_posts[nrand].url).read())
+        # Image.open(path).show()
+
+        ImageAddress = path
+        ImageItself = Image.open(ImageAddress)
+        ImageNumpyFormat = np.asarray(ImageItself)
+        plt.imshow(ImageNumpyFormat)
+        plt.draw()
+        plt.pause(510) # pause how many seconds
+        plt.close()
+
+        return hot_posts[nrand]
+
+
+
 if __name__ == '__main__':
     r = rpie_reddit()
+
+    _ = r.get_smol_cat()
 
     print(r.get_dadjoke())
     print(f'{datetime.datetime.now()} | Done.')
